2ee0c8608457fa01fa1fbaa675494b5724f48312
abril650 <purxpleabrill@gmail.com>
2023-07-12 14:38:27 -0700

    Initial Commit

---
+++ My first Project/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  My first Project
+//
+//  Created by scholar on 7/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+            Text("Here's a very cute panda")
+                .font( .title)
+            
+            
+            Image(/*@START_MENU_TOKEN@*/"Image Name"/*@END_MENU_TOKEN@*/)
+        }
+
+        
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ My first Project/My_first_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_first_ProjectApp.swift
+//  My first Project
+//
+//  Created by scholar on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct My_first_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
